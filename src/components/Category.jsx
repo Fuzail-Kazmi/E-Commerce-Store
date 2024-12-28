c86f86b69bbf45a37a0293a0ba4f9019583db125
Abbas kazmi <fuzail.codecare@gmail.com>
2024-12-28 22:03:06 +0500

    category-sec add

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaAngleRight } from "react-icons/fa6";
 
 export const Category = () => {
+  const categoryBoxData = [
+    {
+      id: "1",
+      category: "electronics",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/1.jpg",
+    },
+    {
+      id: "2",
+      category: "beauty",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/3.jpg",
+    },
+    {
+      id: "3",
+      category: "toys",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/2.jpg",
+    },
+
+    {
+      id: "4",
+      category: "clothes",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/1.jpg",
+    },
+    {
+      id: "1",
+      category: "electronics",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/1.jpg",
+    },
+    {
+      id: "2",
+      category: "beauty",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/3.jpg",
+    },
+    {
+      id: "3",
+      category: "toys",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/2.jpg",
+    },
+
+    {
+      id: "4",
+      category: "clothes",
+      image: "https://mobirise.com/extensions/commercem4/assets/images/1.jpg",
+    },
+   
+  ];
+
   return (
-    <div>Category</div>
-  )
-}
+    <>
+      <div className="categories-container">
+        <div className="categories-wrapper p-3">
+          <div className="categories-head flex space-between ">
+            <div className="categories-head__heading text-lg font-semibold">
+              Categories
+            </div>
+            <div>
+              <NavLink
+                to="/products"
+                className="text-sm right-arrow flex items-center cursor-pointer"
+              >
+                Shop More <FaAngleRight />
+              </NavLink>
+            </div>
+          </div>
+          <div className="categories-main__container">
+            {categoryBoxData.map((item) => {
+              return (
+                <div className="categories-main" key={item.id} id={item.id}>
+                  <div className="categories-items">
+                    <div
+                      style={{ backgroundImage: `url(${item.image})` }}
+                    ></div>
+                  </div>
+                  <div className="categories-title text-center">
+                    {item.category || "No Category"}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
